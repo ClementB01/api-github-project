e9a3f229e4597ea0e2df9bd2b488ca5d8fd25bf4
ClementB01 <bar.clement01@gmail.com>
2020-04-18 09:52:19 +0200

    First commit

---
+++ app/src/main/java/fr/bar/template/MainActivity.kt
@@ -0,0 +1,13 @@
+package fr.bar.template
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.appsolute.template.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
